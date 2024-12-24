42241082325b3715e47f46692d133c7eba7b46e5
raihanuldev <rihanthedev@gmail.com>
2024-12-24 23:11:25 +0600

    codechef "C++"

---
+++ codeChef/new.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // cout << "Hello, World";
+    // cout << 5 + 3 / 2;
+    // cout << "Hello, World';
+    // cout<<9*12;
+    // cout <<"Learn Coding on CodeChef";
+    // cout << "Code" << "Chef";
+    // cout << 5 << " " << 5;
+    // cout << 7 << "\n" << 5;
+    return 0;
+}
